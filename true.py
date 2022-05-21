191b8680cfe9ed2b5601c53b02045a076811074b
GalchenkoS <105945730+GalchenkoS@users.noreply.github.com>
2022-05-21 15:06:08 +0300

    and if the numbers are true

---
+++ true.py
@@ -0,0 +1,78 @@
+def my_function():
+  a = int(input("введите цифру a:"))
+  b = int(input("введите цифру b:"))
+  c = int(input("введите цифру c:"))
+  if a > b:
+    print('грустный текст')
+  elif b > a and b > c:
+    print('безгранично happy text')
+  elif a == b:
+    print('равные цифры')
+my_function()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
